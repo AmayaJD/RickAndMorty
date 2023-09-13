27de6686efc46e522bded57386249e4d0b6557ac
Jose amaya <jose_1341986@hotmail.com>
2023-09-13 00:35:04 -0400

    option menu

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,57 @@ import LocationInfo from './components/LocationInfo'
 import ResidentsCard from './components/ResidentsCard'
 import Loader from './components/Loader'
 import PageCard from './components/PageCard'
-
+// Earth (Replacement Dimension) que posicion es este?
 function App() {
 
   const [inputValue, setInputValue] = useState(getRandomNumber(126))
-  
-  const url = `https://rickandmortyapi.com/api/location/${inputValue || 'Error'}`
+  const url = `https://rickandmortyapi.com/api/location/${inputValue}`
   const [location, getLocation, hasError, isLoading ] = useFetch(url)
-
   const [firstItem, setFirstItem] = useState(1)
   const [lastItem, setLastItem] = useState(8)
-
+  const [currentLocation, setCurrentLocation] = useState(null)
+  const [locationsList, setLocationsList] = useState([])
+  const [errorMessage, setErrorMessage] = useState("Hey! you must provide an id from 1 to 126 or the location's name ❌")
   const lastIndex = firstItem * lastItem
   const firstIndex = lastIndex - lastItem
-
   let quantyty = 8
-
-  let residents = location?.residents.slice(firstIndex,lastIndex)
-  let qtyResidts = location?.residents.length
-
-    
-  useEffect(() => {
-    getLocation()
-  }, [inputValue])
+  let residents = currentLocation?.residents.slice(firstIndex,lastIndex)
   
-  const inputSearch = useRef()
+    useEffect(() => {
+      getLocation()
+    }, [inputValue])
+    
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    setInputValue(inputSearch.current.value.trim())
-    inputSearch.current.value = ""
-    setFirstItem(1) 
-  }
+    useEffect(() => {
+      setLocationsList([])
+    }, [currentLocation])
+
+
+    useEffect( () => {
+      let tempCurrentLocation = location 
+      if ( tempCurrentLocation !== undefined && tempCurrentLocation.hasOwnProperty('results')) {
+        if (location.results.length > 1) {
+          setLocationsList(location.results)
+        } else {
+          setCurrentLocation(location.results[0])
+        }
+      } else {
+        setCurrentLocation(tempCurrentLocation)
+      }    
+    }, [location])
+
+    const inputSearch = useRef()
+
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      let searchString = inputSearch.current.value.trim()
+    if (!Number(searchString)) {
+      searchString = `?name=${searchString}`
+    }
+    setInputValue(searchString)
+      inputSearch.current.value = ""
+      setFirstItem(1) 
+    }
 
 
   return (
@@ -54,37 +73,46 @@ function App() {
           <button className='btn__Search'>Search</button>
         </form>
 
+
+        <div className='menu_option'>
+          {
+            locationsList.map((location, i) => {
+                return <button className='pagination__btn' key={i} onClick={ () => setCurrentLocation(location)}> {location.name} </button>
+            })
+          }
+        </div>
+
         {
         isLoading 
         ? <Loader/>
         :
         (
           hasError
-        ? <h2 className='hasErr'>Hey! you must provide an id from 1 to 126 ❌</h2>
-        : <>
+          ? <h2 className='hasErr'>{ errorMessage }</h2>
+          : <>
 
         <LocationInfo
-        location={location}
+        location={currentLocation}
         />
 
         <div className='container_card'> 
           {
-          residents?.map(residentUrl => (
-          <ResidentsCard
-          key={residentUrl}
-          residentUrl={residentUrl}
-          />
-          ))
+            residents?.map(residentUrl => (
+              <ResidentsCard
+                key={residentUrl}
+                residentUrl={residentUrl}
+                />
+            ))
           }
         </div>
         
         <PageCard
-        quantyty={quantyty} 
-        setFirstItem={setFirstItem}
-        firstItem={firstItem}
-        lastItem={lastItem}
-        setLastItem={setLastItem}
-        qtyResidts={location?.residents.length}
+          quantyty={quantyty} 
+          setFirstItem={setFirstItem}
+          firstItem={firstItem}
+          lastItem={lastItem}
+          setLastItem={setLastItem}
+          qtyResidts={currentLocation?.residents.length}
         />
 
         </>
